3a1d962c207a58a5fcca920ec06d7cee4847d62f
liji10 <liji10@baidu.com>
2025-12-01 10:44:17 +0800

    [KUNLUNXIN] add comment for threshold

--- benchmark/test_generic_pointwise_perf.py
+++ benchmark/test_generic_pointwise_perf.py
@@ -136,9 +136,10 @@ def addcdiv_input_fn(shape, cur_dtype, device):
 )
 def test_generic_pointwise_benchmark(op_name, torch_op, input_fn, dtypes):
     if vendor_name == "kunlunxin" and SkipVersion("torch", "<2.5"):
-        # torch aten 2.0 supports threshold but not for float16
         if op_name in ["threshold"]:
-            pytest.skip("TODOFIX")
+            pytest.skip(
+                "kunlunxin torch aten 2.0 supports threshold but not for float16"
+            )
     bench = GenericBenchmark(
         input_fn=input_fn, op_name=op_name, torch_op=torch_op, dtypes=dtypes
     )
